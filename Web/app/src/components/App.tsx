2b38f16c21016284be510068d1ac32970a6265af
Delsaut Pierre-Arnaud <pa.delsaut@wavenet.be>
2020-04-30 14:00:26 +0200

    Add Ingredients.tsx

--- Web/app/src/components/App.tsx
+++ Web/app/src/components/App.tsx
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/esm/Container";
 import { hot } from "react-hot-loader/root";
 import Ingredients from "./Ingredients";
 
-const getComponent = (pathname: string) => (pathname ? <Home /> : <Ingredients />);
+const getComponent = (pathname: string) => (pathname === "add-ingredient" ? <Ingredients /> : <Home />);
 
 const App = () => (<Container>{getComponent(window.location.pathname)}</Container>);
 
