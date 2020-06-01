e2a820cb4ee4773a714d43a35bcd71b3b5f04e4f
diplomegalo@hotmail.com <diplomegalo@hotmail.com>
2020-06-01 11:17:34 +0200

    split form and manage form

--- Web/app/src/components/App.tsx
+++ Web/app/src/components/App.tsx
@@ -8,10 +8,10 @@ import {
     NavLink,
 } from "react-router-dom";
 import Vegetable from "components/Vegetable";
+import Appellation from "components/Appellation";
 import Home from "./Home";
 import About from "./About";
 import NotFound from "./NotFound";
-import RouteComponent from "./RouteComponent";
 
 const App = () => (
     <Router>
@@ -23,16 +23,19 @@ const App = () => (
                 <NavLink className="nav-link" activeClassName="active" to="vegetable">Vegetable</NavLink>
             </Nav.Item>
             <Nav.Item>
+                <NavLink className="nav-link" activeClassName="active" to="appellation">Appellation</NavLink>
+            </Nav.Item>
+            <Nav.Item>
                 <NavLink className="nav-link" activeClassName="active" to="about">About</NavLink>
             </Nav.Item>
         </Nav>
-
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/home" exact component={Home} />
             <Route path="/about" component={About} />
             <Route path="/vegetable/:name" component={Vegetable} />
             <Route path="/vegetable" component={Vegetable} />
+            <Route path="/appellation" component={Appellation} />
             <Route component={NotFound} />
         </Switch>
     </Router>
