fa3884b4479035e558ca575b83b800cd26d25d07
Delsaut Pierre-Arnaud <pa.delsaut@wavenet.be>
2020-07-06 17:44:05 +0200

    Add form validation

--- Web/app/src/components/AppellationForm.tsx
+++ Web/app/src/components/AppellationForm.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Form from "react-bootstrap/esm/Form";
 import Button from "react-bootstrap/esm/Button";
 import IAppellation from "models/IAppellation";
+import { OnSubmit, useForm } from "react-hook-form";
+import WineServiceApi from "../servicesApi/WineServiceApi";
 
 export interface IAppellationProps
 {
@@ -10,13 +12,19 @@ export interface IAppellationProps
 
 const AppellationForm = (props: IAppellationProps) =>
 {
-    const { appellation } = props;
+    const {
+        register, handleSubmit, errors, formState,
+    } = useForm<IAppellation>();
+
+    const { isSubmitted } = formState;
+
+    const submit = (data: any) => console.log(data);
 
     return (
-        <Form id="registerAppellation">
+        <Form id="registerAppellation" onSubmit={handleSubmit(submit)}>
             <Form.Group>
                 <Form.Label>Nom :</Form.Label>
-                <Form.Control as="input" type="text" />
+                <Form.Control as="input" type="text" name="name" ref={register({ required: true })} isInvalid={!!errors.name} />
             </Form.Group>
             <Form.Group>
                 <Form.Label htmlFor="vineyard">Vignoble :</Form.Label>
@@ -37,12 +45,14 @@ const AppellationForm = (props: IAppellationProps) =>
             <Form.Group>
                 <Form.Label htmlFor="type">Type :</Form.Label>
                 <Form.Control as="select" id="type">
-                    <option>Vignoble</option>
+                    <option>Type</option>
                     <option value="1">AOC | AOP</option>
                     <option value="2">IGP</option>
                 </Form.Control>
             </Form.Group>
-            <Button type="submit">Ajouter</Button>
+            <Form.Group>
+                <Button type="submit">Ajouter</Button>
+            </Form.Group>
         </Form>
     );
 };
